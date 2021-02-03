2369cbf8d9020f562f3f118feff08edba5353730
barisanik <31242517+barisanik@users.noreply.github.com>
2021-02-03 10:26:50 +0300

    Update settings.py

--- TestBlogDjango/settings.py
+++ TestBlogDjango/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'YOUR_SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0'] 
+ALLOWED_HOSTS = ['127.0.0.1', 'www.something.com', 'something.com'] # Use 127.0.0.1 for local usage. Use your domain for server.
 
 
 # Application definition
