0bf122ba501d204ac419b16819795ba0c7e4cb55
barisanik <31242517+barisanik@users.noreply.github.com>
2021-02-03 10:19:23 +0300

    Update admin.py

--- article/admin.py
+++ article/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Article 
+from .models import Article # Article model
 
 # Register your models here.
+# This is for adding article model into admin dashboard
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ["title", "author", "created_date"]
