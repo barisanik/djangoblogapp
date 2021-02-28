b8373e3e679f363738d2baecce6b5b309cf28eca
barisanik <31242517+barisanik@users.noreply.github.com>
2021-02-28 09:28:27 +0300

    User follow model included

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class UserFollow(models.Model):
+    id = models.AutoField(primary_key=True)
+    followerID = models.CharField(max_length=10, verbose_name="Takip Eden")
+    personID = models.CharField(max_length=10, verbose_name="Takip Edilen")
+    follow_date = models.DateTimeField(auto_now_add=True, verbose_name="Follow Date")
