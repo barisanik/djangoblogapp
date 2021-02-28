20c70af489ea9ff4168078a23499cb77eb6a8f0e
barisanik <31242517+barisanik@users.noreply.github.com>
2021-02-28 09:39:01 +0300

    Upvote url included

--- article/urls.py
+++ article/urls.py
@@ -6,5 +6,8 @@ app_name = "article"
 urlpatterns = [
     path('detail/<int:id>', views.articleDetailPage, name="articleDetailPage"),
     path('addarticle/', views.addArticlePage, name="addArticlePage"),
+    path('edit/<int:id>', views.editArticlePage, name="editArticlePage"),
+    path('delete/<int:id>', views.deleteArticlePage, name="deleteArticlePage"),
     path('all/', views.allArticlePage, name="allArticlePage"),
+    path('upvote/<int:id>', views.articleUpvote, name="articleUpvote"),
 ]
