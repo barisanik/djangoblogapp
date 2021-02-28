7b4cb31a93972187ffcd89f8a995ecccd18dec64
barisanik <31242517+barisanik@users.noreply.github.com>
2021-02-28 09:38:06 +0300

    Update models.py

--- article/models.py
+++ article/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-# Create your models here.
-class Article(models.Model):
-    author = models.ForeignKey("auth.User", on_delete= models.CASCADE, verbose_name="Yazar") # 'on_delete' will help about deleting articles from related user.
+#
+class Article(VoteModel, models.Model):
+    author = models.ForeignKey("auth.User", on_delete= models.CASCADE, verbose_name="Yazar") 
     title = models.CharField(max_length=50, verbose_name="Başlık")
     content = models.TextField(verbose_name="İçerik")
     is_public = models.BooleanField(default=1, verbose_name="Makale gizliliği herkese açık olarak kalsın.")
     created_date = models.DateTimeField(auto_now_add=True, verbose_name="Oluşturulma Tarihi")
+    last_edit_date = models.DateTimeField(auto_now=True, verbose_name="Oluşturulma Tarihi")
     def __str__(self):
         return self.title
